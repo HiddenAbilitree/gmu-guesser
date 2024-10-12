14e19fe27912b42e72df1c0b8fac08fcdc29d74a
Orion Tran <oriontrandev@gmail.com>
2024-10-12 16:38:06 -0400

    smooth

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 export const Map = () => {
   // create a ref of the map
   const mapRef = useRef<maplibregl.Map | null>(null);
+  const [mapAnimating, setMapAnimating] = useState(false);
 
   useEffect(() => {
     const map = new maplibregl.Map({
@@ -13,6 +14,9 @@ export const Map = () => {
       zoom: 16, // starting zoom
       minZoom: 14,
       maplibreLogo: false,
+      attributionControl: {
+        compact: true,
+      },
       maxBounds: [
         [-77.31206068310384, 38.824673633776136],
         [-77.30218251510037, 38.83486941965788],
@@ -23,11 +27,26 @@ export const Map = () => {
     return () => mapRef.current?.remove();
   }, []);
 
+  useEffect(() => {
+    const frame = () => {
+      const ref = mapRef.current;
+      if (!mapAnimating || !ref) return;
+
+      ref.resize();
+      requestAnimationFrame(frame);
+    };
+
+    const animator = requestAnimationFrame(frame);
+    return () => cancelAnimationFrame(animator);
+  }, [mapAnimating]);
+
   return (
     <div className="absolute z-50 transition-all w-[30vw] h-[30vh] bottom-5 hover:w-[50vw] hover:h-[50vh] right-5">
       <div
         id="map"
         className="w-full h-full shadow-md opacity-60 hover:opacity-100 rounded-3xl hover:rounded-lg"
+        onAnimationStart={() => setMapAnimating(true)}
+        onAnimationEnd={() => setMapAnimating(false)}
       />
     </div>
   );
