d7e62e56f3a9b065864ef0927f5e14cf80c06ac4
nfpog <danhuynh307@gmail.com>
2024-10-13 00:03:35 -0400

    Update data.ts

--- src/datum/data.ts
+++ src/datum/data.ts
@@ -2,69 +2,93 @@ import { MapData } from '@/Types';
 
 export const maps: MapData[] = [
   {
-    locationName: 'MIX Lab',
+    locationName: 'MIX Lab in Horizon Hall',
     latlong: [38.8310979, -77.3076892],
     panorama: 'IMG_4077.jpg',
+    description: 'The MIX, or Mason Innovative Exchange, is a space for students and staff to create and explore.'
+    + ' It offers various programs and events, as well as a machine inventory of 3D Printers and Scanners, Laser/Metal cutting, Resin Printing, Wood Shop, and so much more!' 
+    + 'Visit https://www.mix.gmu.edu/ to find out more.'
   },
   {
-    locationName: 'Horizon Floor Four Window',
+    locationName: 'Horizon Hall Floor Four Window',
     latlong: [38.831831, -77.308017],
     panorama: 'pic1.jpg',
+    description: 'Easter Egg! We worked on most of our project here, a very comfortable working space.'
   },
   {
     locationName: 'Fenwick Library D-Wing Entrance',
     latlong: [38.831318, -77.306197],
     panorama: 'pic2.jpg',
+    description: 'First built in 1967, this central library holds 1.5 million books, in addition to having various research and technological resources.'
+    + 'It serves as a great study space, especially with the Argo Tea Cafe inside. More info: https://library.gmu.edu/locations/fenwick'
   },
   {
     locationName: 'Corner of a Bridge behind Dominion',
     latlong: [38.833044, -77.305026],
     panorama: 'pic3.jpg',
+    description: 'Just a relaxing spot'
   },
   {
     locationName: 'Table Outside The Spot',
     latlong: [38.83346881, -77.30505978],
     panorama: 'pic4.jpg',
+    description: 'A common, scenic hangout area after grabbing food from The Spot. The spot is known for their great vegan and vegeterian options.'
   },
   {
-    locationName: 'Disc Gold Field Behind The Hub',
+    locationName: 'Disc Golf Field Behind The Hub',
     latlong: [38.83032822, -77.30418394],
     panorama: 'pic5.jpg',
+    description: 'Niche location to play disc golf with friends! Also next to The Hub, a common area for meetings, events, and games for students.'
+    + 'Check out upcoming activities: https://studentcenters.gmu.edu/hub/'
   },
   {
     locationName: 'Volleyball by Dorms',
     latlong: [38.833539, -77.305786],
     panorama: 'dan pan1.jpg',
+    description: 'One of the many dorm buildings on campus. Each floor is decorated with a fun theme! Some of our favorites include Pokemon, Adventure Time, and Spongebob'
   },
   {
     locationName: 'Population Health Center',
     latlong: [38.834277, -77.30966],
     panorama: 'dan pan2.jpg',
+    description: 'This center gives interprofessional care to underserved populations as well as researches to improve public health.'
+    + 'Furthermore, there are lots of learning opportunities for those interested. Learn more: https://publichealth.gmu.edu/about/population-health-center'
   },
   {
     locationName: 'Outside Exploratory Hall',
     latlong: [38.830541, -77.305904],
     panorama: 'dan pan5.jpg',
+    description: 'A science center with "164,050 square feet of new labs, classrooms, and study areas. "   '
+    + 'If you look closely, there are actually molecular deisgns on the stainless steel structure of the building. '
+    + 'Learn more: https://www.azahner.com/works/exploratory-hall-at-george-mason-university '
   },
   {
-    locationName: 'Outside Engineering Building and Aquatic Fitness Center',
+    locationName: 'Outside CEC Building and Aquatic Fitness Center',
     latlong: [38.827751, -77.304857],
     panorama: 'dan pan6.jpg',
+    description: 'Built in the 1980s, the Engineering building has continued to develop and evolve alongside technology. '
+    + '"HERD Rankings in top 100 for Engineering and Computing. Computing highest in Virginia" '
+    + 'Learn more: https://cec.gmu.edu/about/news-and-information/brief-history-college'
   },
   {
     locationName: 'Enterprise Bus Stop',
     latlong: [38.829153, -77.305394],
     panorama: 'dan pan7.jpg',
+    description: 'Where primary transportation services occur. Also next to Enterprise Hall, a frequent area for classes, consisting of 5 floors'
   },
   {
-    locationName: 'Munitas Statue by JC',
+    locationName: 'Communitas Statue by JC',
     latlong: [38.829136, -77.30867],
     panorama: 'dan pan8.jpg',
+    description: '"Communitas," bronze statue by artist Azriel Awret on the Fairfax Campus (https://www.gmu.edu/discover-mason). '
+    + 'Seen often when walking from parking to JC '
   },
   {
     locationName: 'Mason Statue by JC',
     latlong: [38.830352, -77.308433],
     panorama: 'dan pan10.jpg',
+    description: 'Iconic statue of George Mason, popular among students as he is dressed up based on upcoming events. proposed in 1989. This bronze statue cost between 30k-50k, becoming a notable landmark for students on campus. '
+    + 'Between JC and deLaski Performing Arts Building'
   },
   {
     locationName: 'Bridge Behind the Hub',
